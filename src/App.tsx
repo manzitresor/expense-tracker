cdea64bd9e24fef713d76e2ddf181ee04bbbfe39
manzitresor <manzitre@gmail.com>
2024-08-02 15:57:54 +0200

    Add expense list component

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,18 @@
+import { useState } from "react"
 import ExpenseForm from "./components/ExpenseForm"
+import ExpenseList from "./components/ExpenseList"
 function App() {
+  const [exprenses,setExpenses] = useState(
+    [
+      {id:1, description: "aaa", amount: 12, category:"Glocaries"},
+      {id:2, description: "ccc", amount: 14, category:"Glocaries"},
+      {id:3, description: "ddd", amount: 15, category:"Glocaries"},
+    ]
+  )
   return (
     <>
     <ExpenseForm/>
+    <ExpenseList expenses={exprenses} onDelete={(id)=> setExpenses(exprenses.filter((el)=> el.id !== id))}/>
     </>
   )
 }
---
+++ src/components/ExpenseList.tsx
@@ -0,0 +1,47 @@
+interface Expense {
+  id: number,
+  description: string,
+  amount: number,
+  category: string,
+}
+
+interface Props {
+  expenses: Expense[];
+  onDelete: (id:number)=> void;
+}
+
+export default function ExpenseList({expenses,onDelete}: Props) {
+  return (
+    <>
+        <table className="table table-bordered border-primary">
+          <thead className="table-dark">
+            <tr>
+              <td>Description</td>
+              <td>Amount</td>
+              <td>Category</td>
+            </tr>
+          </thead>
+          <tbody>
+            {expenses.map((expense) => 
+                  <tr key={expense.id}>
+                      <td>{expense.description}</td>
+                      <td>{expense.amount}</td>
+                      <td>
+                        <span  className="category">{expense.category}</span>
+                      <button className="btn btn-outline-danger" id="delete-btn" onClick={()=> onDelete(expense.id)}>Delete</button>
+                      </td>
+                  </tr>
+            )}
+         
+        
+          </tbody>
+          <tfoot>
+            <tr>
+              <td className="total">Total</td>
+              <td className="amount">${expenses.reduce((acc,expense) => expense.amount + acc,0)}</td>
+            </tr>
+          </tfoot>
+        </table> 
+    </>
+  )
+}
