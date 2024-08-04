29de1ee3507b5055c2bb093cb94de37c7f221e1f
manzitresor <manzitre@gmail.com>
2024-08-04 22:55:19 +0200

    Add functionality to filter expense list

--- src/App.tsx
+++ src/App.tsx
@@ -3,18 +3,22 @@ import ExpenseForm from "./components/ExpenseForm";
 import ExpenseList from "./components/ExpenseList";
 import ExpenseFilter from "./components/ExpenseFilter";
 function App() {
-  const [exprenses,setExpenses] = useState(
+  const [selectedExpense,setSelectedExpense] = useState('')
+  const [expenses,setExpenses] = useState(
     [
-      {id:1, description: "aaa", amount: 12, category:"Glocaries"},
-      {id:2, description: "ccc", amount: 14, category:"Glocaries"},
-      {id:3, description: "ddd", amount: 15, category:"Glocaries"},
+      {id:1, description: "aaa", amount: 12, category:"Gloceries"},
+      {id:2, description: "ccc", amount: 14, category:"Gloceries"},
+      {id:3, description: "ddd", amount: 15, category:"Utilities"},
     ]
   )
+  const visibility = selectedExpense ? expenses.filter((expense => expense.category === selectedExpense)): expenses
   return (
     <>
-    <ExpenseForm/>
-    <ExpenseList expenses={exprenses} onDelete={(id)=> setExpenses(exprenses.filter((el)=> el.id !== id))}/>
-    <ExpenseFilter/>
+      <ExpenseForm/>
+      <div className="mb-3">
+        <ExpenseFilter onSelect={(category)=> setSelectedExpense(category)}/>
+      </div>
+      <ExpenseList expenses={visibility} onDelete={(id)=> setExpenses(expenses.filter((el)=> el.id !== id))}/>
     </>
   )
 }
--- src/components/ExpenseFilter.tsx
+++ src/components/ExpenseFilter.tsx
@@ -1,8 +1,17 @@
 
-export default function ExpenseFilter() {
+interface Props {
+  onSelect: (category: string) => void
+}
+
+export default function ExpenseFilter({onSelect}: Props) {
   return (
     <>
-        <h1>Expense Filter component</h1>
+        <select className="form-select" onChange={(event)=> onSelect(event.target.value)}>
+          <option value="">All categories</option>
+          <option value="Gloceries">Gloceries</option>
+          <option value="Utilities">Utilities</option>
+          <option value="Entertainment">Entertainment</option>
+        </select>
     </>
   )
 }
